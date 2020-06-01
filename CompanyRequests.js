d4afa79904acb1ebe26afa06dfda7469eed3a5c0
danylboiko95 <44903844+danylboiko95@users.noreply.github.com>
2020-06-01 15:40:09 +0300

    Add files via upload

---
+++ CompanyRequests.js
@@ -0,0 +1,106 @@
+import React, {useEffect} from 'react';
+import LayoutPage from "../../hoc/LayoutPage/LayoutPage";
+import Table from "../../components/Table/Table";
+import {DeadlineCell, EditCell, StatusCell, UserCell} from "../../components/TableCells/TableCells";
+import SelectColumnFilter from "../../components/Table/Filters/SelectColumnFilter";
+import * as actions from "../../store/actions";
+import WithErrorHandler from "../../hoc/WithErrorHandler/WithErrorHandler";
+import {connect} from "react-redux";
+import axios from "../../apis/axios";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import DateColumnFilter from "../../components/Table/Filters/DateColumnFilter";
+
+const CompanyRequests = (props) => {
+    useEffect(() => {
+        props.onRequestsGet(props.companies.fetchedCompany._id
+            ? props.companies.fetchedCompany._id
+            : props.authUser.user.company);
+        props.onCompanyEmployeeGet(props.authUser.user.company, localStorage.getItem('token'))
+    }, []);
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Deadline',
+                accessor: 'deadlineAt',
+                Cell: DeadlineCell,
+                filter: (rows, id, filterValue) => {
+                    return rows.filter(row => {
+                        const rowValue = row.values[id];
+                        return filterValue > rowValue
+                    });
+                },
+                Filter: DateColumnFilter
+            },
+            {
+                Header: 'Assigned',
+                accessor: 'assigned',
+                disableFilter: true,
+                Cell: UserCell
+            },
+            {
+                Header: 'Assistant',
+                accessor: 'assistant',
+                disableFilter: true,
+                Cell: UserCell,
+            },
+            {
+                Header: "Status",
+                accessor: "status",
+                filter: (rows, id, filterValue) => {
+                    return rows.filter(row => {
+                        const rowValue = row.values[id];
+                        return filterValue.includes(rowValue);
+                    });
+                },
+                Cell: StatusCell,
+                Filter: SelectColumnFilter
+            },
+            {
+                Header: 'Description',
+                accessor: 'description',
+                disableFilter: true,
+            },
+            {
+                Header: '',
+                accessor: 'editColumn',
+                Cell: EditCell,
+                disableSortBy: true,
+                disableFilter: true,
+            },
+        ],
+        []
+    );
+
+    const data = React.useMemo(() =>
+            props.requests.fetchedRequests
+        , [props.requests.fetchedRequests]);
+    return (
+        props.requests.loaded ? (
+                <LayoutPage title="Comment">
+                    <Table visibleFields={['deadlineAt', 'assigned']}
+                           shouldResponse
+                           showFilter
+                           columns={columns}
+                           data={props.requests.fetchedRequests.length ? data : []}/>
+                </LayoutPage>)
+            : <Spinner/>
+    )
+};
+
+const mapStateToProps = (state) => {
+    return {
+        requests: state.requests,
+        companies: state.companies,
+        authUser: state.auth
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onRequestsGet: (companyId, token) => dispatch(actions.fetchRequests(companyId, token)),
+        onCompanyEmployeeGet: (companyId, token) => dispatch(actions.fetchEmployees(companyId, token))
+    }
+};
+
+export default WithErrorHandler(connect(mapStateToProps, mapDispatchToProps)(CompanyRequests), axios);
